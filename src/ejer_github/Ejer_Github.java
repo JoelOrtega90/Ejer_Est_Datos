829e045c43dcb1a7cff978969e03d2f860d23ad9
Joel <Joel@DESKTOP-RJC88FC>
2021-04-19 14:52:12 -0600

    Primer programa que muestra saludos

---
+++ src/ejer_github/Ejer_Github.java
@@ -0,0 +1,20 @@
+/*
+ Proyecto generado en netbeans para trabajar programas que se puedan visualizarlos
+en github
+ */
+package ejer_github;
+
+/**
+ *
+ * @author Joel
+ */
+public class Ejer_Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola soy Joel Ortega");
+    }
+    
+}
